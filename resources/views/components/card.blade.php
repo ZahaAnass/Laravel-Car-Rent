1d92749e5ddb48ee2385a6aa7d7a69effa782a12
ZahaAnass <zahaanass277@gmail.com>
2025-08-29 17:41:57 +0100

    add card-components

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,18 +1,36 @@
 @props([
-    "color",
-    "bgColor" => "white",
+    'color' => 'black', // default color if not provided
+    'bgColor' => 'white', // default background color if not provided
 ])
 
-<div {{ $attributes
-            ->merge(["lang" => "ar"])
-            ->class("card card-text-$color card-bg-$bgColor") }}>
-    <div {{ $title->$attributes->class("card-header") }}>
-        {{ $title }}
-    </div>
-    @if ($slot->isEmpty())
-        <p>Please provide some content</p>
-    @else
-        {{ $slot }}
+@php
+    $attributes = $attributes->merge([
+        'class' => "card card-text-{$color} card-bg-{$bgColor} " . ($attributes->get('class') ?? '')
+    ]);
+    
+    if (!$attributes->has('lang')) {
+        $attributes = $attributes->merge(['lang' => 'ar']);
+    }
+@endphp
+
+<div {{ $attributes }}>
+    @if(isset($title))
+        <div {{ $title->attributes->class("card-header") }}>
+            {{ $title }}
+        </div>
     @endif
-    <div class="card-footer">{{ $footer }}</div>
+    
+    <div class="card-body">
+        @if ($slot->isEmpty())
+            <p>Please provide some content</p>
+        @else
+            {{ $slot }}
+        @endif
+    </div>
+    
+    @if(isset($footer))
+        <div {{ $footer->attributes->class("card-footer") }}>
+            {{ $footer }}
+        </div>
+    @endif
 </div>
