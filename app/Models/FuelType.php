7947e0962fe4b27a3e031c4cd875146ace34535a
ZahaAnass <zahaanass277@gmail.com>
2025-11-01 00:17:44 +0100

    creating data models

---
+++ app/Models/FuelType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FuelType extends Model
+{
+    use HasFactory; // laravel will assume table name is fuel_types
+
+    //protected $table = 'fuel_types';
+    //protected $primaryKey = 'code'; // set primary key to 'code'
+    //public $incrementing = false; // make incrementing false for string primary key
+    //protected $keyType = 'string'; // set key type to string
+
+    //const CREATED_AT = 'created_date';
+    //const UPDATED_AT = null;
+
+    public $timestamps = false; // disable timestamps
+
+
+}
