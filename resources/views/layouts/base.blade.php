3bddc6d084ee663164ec3f9f9df843cd0f9c00c3
ZahaAnass <zahaanass277@gmail.com>
2025-10-31 15:22:36 +0100

    replace hardcoded link with routes name

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -27,7 +27,7 @@
       rel="stylesheet"
     /> -->
 
-    <link rel="stylesheet" href="css/app.css" />
+    <link rel="stylesheet" href="/css/app.css" />
     <!-- <link rel="stylesheet" href="css/output.css" /> -->
   </head>
   <body @isset($bodyClass) class="{{ $bodyClass }}" @endisset>
@@ -40,7 +40,7 @@
       crossorigin="anonymous"
       referrerpolicy="no-referrer"
     ></script>
-    <script src="js/app.js"></script>
+    <script src="/js/app.js"></script>
   </body>
 </html>
 
