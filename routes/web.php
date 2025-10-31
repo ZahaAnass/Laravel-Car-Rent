102b54dbbcbf9d6e23aadcd0deaec83034537e34
ZahaAnass <zahaanass277@gmail.com>
2025-10-31 15:19:44 +0100

    add car routes

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,13 @@ use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SignupController;
+use App\Http\Controllers\CarController;
 
 Route::get("/", [HomeController::class, "index"])
 ->name("home");
+
+Route::get("/car/search", [CarController::class, "search"])->name("car.search");
+Route::resource("car", CarController::class);
 
 Route::get("/signup", [SignupController::class, "create"])
 ->name("signup");
