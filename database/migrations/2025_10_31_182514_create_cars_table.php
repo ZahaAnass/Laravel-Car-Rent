581b56ed510bb08bcb215d058a86e1d98e490947
ZahaAnass <zahaanass277@gmail.com>
2025-11-19 19:41:25 +0100

    add delete cascade on user foreign key

--- database/migrations/2025_10_31_182514_create_cars_table.php
+++ database/migrations/2025_10_31_182514_create_cars_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->integer("mileage");
             $table->foreignId("car_type_id")->constrained("car_types");
             $table->foreignId("fuel_type_id")->constrained("fuel_types");
-            $table->foreignId("user_id")->constrained("users");
+            $table->foreignId("user_id")->constrained("users")->onDelete("cascade");
             $table->foreignId("city_id")->constrained("cities");
             $table->string("address", 255);
             $table->string("phone", 45);
